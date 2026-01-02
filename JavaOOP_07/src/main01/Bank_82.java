e606982ff98493d4bdc8f833f41fcc256744727c
pradip <pradippatil1543@gmail.com>
2026-01-03 01:07:07 +0530

    refereactor jave opps code

---
+++ JavaOOP_07/src/main01/Bank_82.java
@@ -0,0 +1,69 @@
+package main01;
+
+public class Bank_82 {
+	
+	private String accountNumber;
+	
+	// instead of int here better to use double because balance should be real number
+	private int accountBalance;
+	private String customerName;
+	private String email;
+	private String phoneNumber;
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public int getAccountBalance() {
+		return accountBalance;
+	}
+	
+	public String customerName() {
+		return customerName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber=accountNumber;
+	}
+	
+	public void setAccountBalance(int accountBalance) {
+		this.accountBalance=accountBalance;
+	}
+	
+	public void setCustomerName(String customerName) {
+		this.customerName=customerName;
+	}
+	
+	public void setEmail(String email) {
+		this.email=email;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber=phoneNumber;
+	}
+	
+	public void depositFund(int depositAmount) {
+		if(depositAmount <0) return;
+		accountBalance+=depositAmount;
+		System.out.println(" Total balance is =>" +accountBalance);
+	}
+	
+	public void withDrawFund(int withDrawAmount) {
+		if(accountBalance-withDrawAmount <0) {
+			System.out.println("Your account balance is low");
+		}
+		else {
+			accountBalance-=withDrawAmount;
+			System.out.println("Your remaing balance is "+ accountBalance);
+		}
+	}
+}
---
+++ JavaOOP_07/src/main01/ConstructorChaining_84_Test.java
@@ -0,0 +1,31 @@
+package main01;
+
+public class ConstructorChaining_84_Test {
+	
+	public static void main(String[] args) {
+//		ConstructorChaining_84 bank=new ConstructorChaining_84();
+//		bank.setAccountBalance(10000);
+//		bank.setAccountNumber("8888");
+//		bank.setCustomerName("Mickal");
+//		bank.setEmail("abc@gmail.com");
+//		bank.setPhoneNumber("123456789");
+		
+	//	ConstructorChaining_84 bank=new ConstructorChaining_84("8888", 100000, "Mickal", "abc@gmail.com", "123456789");
+		ConstructorChaining_84 bank=new ConstructorChaining_84();
+		
+		System.out.println(bank.getAccountBalance());
+		System.out.println(bank.getAccountNumber());
+		bank.depositFund(1000);
+		bank.withDrawFund(100);
+		bank.withDrawFund(2000);
+		bank.depositFund(10);
+		bank.withDrawFund(110);
+		
+		
+		ConstructorChaining_84 timAccount=new ConstructorChaining_84("5555", 1000, "test customer");
+		System.out.println(" here is another customer =  > "+ timAccount.getAccountNumber());
+		timAccount.depositFund(500);
+		timAccount.withDrawFund(300);
+	}
+
+}
