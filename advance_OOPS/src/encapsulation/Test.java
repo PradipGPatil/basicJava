16fef1a8cfbb88712f4e13c01b347c316aec9691
pradip <pradippatil1543@gmail.com>
2026-01-11 23:44:24 +0530

    add string method document

--- advance_OOPS/src/encapsulation/Test.java
+++ advance_OOPS/src/encapsulation/Test.java
@@ -3,8 +3,10 @@ package encapsulation;
 public class Test {
 	
 public static void main(String[] args) {
+	// 1 disadventage- if player did not have constructor then calling code will have responsibility 
+	// initialize the data
 //	Players player=new Players();
-//	// if we change name of the variable to fullName in player class subClass need to chagne the anem
+//	// disadventage 2-  if we change name of the variable to fullName in player class then calling code also need to change the code
 //	player.name="tim";
 //	player.health=20;
 //	player.weapon="sword";
@@ -13,7 +15,7 @@ public static void main(String[] args) {
 //	player.loseHealth(damage);
 //	
 //	System.out.println(" reaminging health "+ player.healthRemaining());
-//	// this condition allow the by pass the check for player health 100 condition 
+//	// disadventage 3 ) this condition allow the by pass the check for player health 100 condition 
 //	player.health=200;
 //	
 //	player.loseHealth(11);
